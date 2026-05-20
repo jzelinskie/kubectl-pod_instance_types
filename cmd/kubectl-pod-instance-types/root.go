7ff747300ce97d3ebee48c0d912b789d0b02c309
Jimmy Zelinskie <jimmy@zelinskie.com>
2026-05-20 15:29:38 -0700

    feat: add cobra command with cli-runtime Factory and PrintFlags

--- cmd/kubectl-pod-instance-types/root.go
+++ cmd/kubectl-pod-instance-types/root.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"context"
+
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/kubectl/pkg/cmd/util"
 )
 
 func newRootCmd(streams genericclioptions.IOStreams) *cobra.Command {
-	return &cobra.Command{Use: "kubectl-pod-instance-types"}
+	configFlags := genericclioptions.NewConfigFlags(true)
+	factory := util.NewFactory(configFlags)
+	printFlags := genericclioptions.NewPrintFlags("").WithTypeSetter(scheme.Scheme)
+
+	var allNamespaces bool
+
+	cmd := &cobra.Command{
+		Use:          "kubectl-pod-instance-types [flags]",
+		Short:        "List pods with their node's cloud instance type",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run(context.Background(), factory, printFlags, allNamespaces, streams)
+		},
+	}
+
+	configFlags.AddFlags(cmd.Flags())
+	printFlags.AddFlags(cmd)
+	cmd.Flags().BoolVarP(&allNamespaces, "all-namespaces", "A", false, "List pods in all namespaces")
+
+	return cmd
 }
--- cmd/kubectl-pod-instance-types/run.go
+++ cmd/kubectl-pod-instance-types/run.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"context"
+
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/kubectl/pkg/cmd/util"
+)
+
+func run(ctx context.Context, factory util.Factory, printFlags *genericclioptions.PrintFlags, allNamespaces bool, streams genericclioptions.IOStreams) error {
+	return nil
+}
